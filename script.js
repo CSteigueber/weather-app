62684ff798c4402c886bb5ee7dfe4da196f0ce25
CSteigueber <c.steigueber@gmail.com>
2019-11-18 12:33:29 +0100

    lunch break

--- script.js
+++ script.js
@@ -1,40 +1,31 @@
 async function CityId  (target){
+    let id=0;
     let arr=target.split(",")
     console.log(arr);
     let pre_list= await fetch("./city.list.json");
     let list= await pre_list.json();
-    console.log(list.length);
-    /*list.forEach(el => {
+    list.forEach(el => {
         if (el.name==arr[0] && el.country==arr[1]){
-            return (el.id);
+            id=el.id;
         }
-    });*/
-    console.log("City not found");
-    return 0;
-
-
-}
-(function (){
-    var id=0;
-    const key="APPID"+"403247d59c49d3321fc77e793af27b59";
-    document.getElementById("run").addEventListener("click", async function (){
-        var target=document.getElementById("target").value;
-        console.log(target);
-        var id= await CityId(target);
-        console.log(id);
+    });
+    if (id==0){
+        console.log("City not found");
+    }
+    return id;
+}    
     
-       /* let url="api.openweathermap.org/data/2.5/forecast?q="+target+"&"+key;
-        var x=fetch(url)
-            .then(()=>{
-                console.log(url);
-                let list=x.json();
-                console.log(list);
-            })
-              
-            .catch(console.log("Damn!"))*/
-      
-        
-    })
+const key="APPID="+"744128119f1ba4097bd9904b7f56efeb";
+document.getElementById("run").addEventListener("click", async function (){
+    var target=document.getElementById("target").value;
+    console.log(target);
+    var id= await CityId(target);
+    console.log("City id: "+ id);
+
+    let url="api.openweathermap.org/data/2.5/forecast?q="+target+"&"+key;
+    var pre_res= await fetch(url)
+    //var res=await pre_res.json();
+    console.table(pre_res);
 
 })
 
