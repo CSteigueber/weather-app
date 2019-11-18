35577da7abdae3a69a4f733e4bcd0f4c8ff3a7e4
CSteigueber <c.steigueber@gmail.com>
2019-11-18 14:58:31 +0100

    mini changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-async function CityId  (target){
+/*async function CityId  (target){
     let id=0;
     let arr=target.split(",")
     console.log(arr);
@@ -13,20 +13,20 @@ async function CityId  (target){
         console.log("City not found");
     }
     return id;
-}    
+}    */
+/* var id= await CityId(target);
+ console.log("City id: "+ id);*/
     
 const key="APPID="+"744128119f1ba4097bd9904b7f56efeb";
 document.getElementById("run").addEventListener("click", async function (){
     var target=document.getElementById("target").value;
     console.log(target);
-   /* var id= await CityId(target);
-    console.log("City id: "+ id);*/
 
     let url="https://api.openweathermap.org/data/2.5/forecast?q="+target+"&"+key;
     var pre_res= await fetch(url)
     var res=await pre_res.json();
-    console.log(pre_res);
-    console.table(res.list);
+    var list=res.list;
+    console.table(list);
 
 })
 
